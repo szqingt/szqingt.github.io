54c856b195bd6d6e18cb3ed22456d2ac160409e3
never_1992 <szqingt@gmail.com>
2023-04-05 15:56:10 +0800

    chore: 1. sitemap  2. rss 3. ui adjust

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,6 +10,7 @@ export async function get(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
+			content: post.body,
 			link: `/blog/${post.slug}/`,
 		})),
 	});
