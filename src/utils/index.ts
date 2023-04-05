85f201ead44346926f273e2ea26b3028f9b7ce89
never_1992 <szqingt@gmail.com>
2023-04-05 17:26:18 +0800

    feat: add estimated reading time

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 let themeMode = localStorage.getItem('color-scheme') || 'auto'
 
-function toggleTheme() {
+export function toggleTheme() {
   const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
 
   function setMode(mode: string) {
@@ -23,7 +23,22 @@ function toggleTheme() {
   setMode('auto')
 }
 
+export function simpleCalcReadingTime(content: string): string {
+  
+  const wordReg = /([\u4E00-\u9FA5])|(\b[a-zA-Z]+\b)|((\b[0-9]+\b))/g
+  const picReg = /!\[[^\]]+\]\([^\)]+\)/g
+  // 阅读时长 = 总字数 ÷ 平均阅读速度（240 wpm）(4 wps)
+  // 图片 per/15s
+  // 视频 todo
+  //：wpm，全称为 Words per minute
+  const wordLength = content.match(wordReg)?.length || 0;
+  const picLength = content.match(picReg)?.length || 0;
+  let sec = 0;
+  sec += wordLength / 4
+  sec += picLength * 15
+  return `${Math.ceil(sec / 60)}min`;
+}
+
 export {
-  themeMode,
-  toggleTheme
+  themeMode
 }
