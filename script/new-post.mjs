c8101b4607f641a8ed2f545a8f87b410995014d2
never_1992 <szqingt@gmail.com>
2023-04-17 19:51:26 +0800

    feat: new post script

---
+++ script/new-post.mjs
@@ -0,0 +1,51 @@
+import { writeFileSync } from 'fs'
+import { argv } from 'node:process';
+import { spawn } from 'child_process'
+
+const BLOG_PATH = 'src/content/blog/'
+const POST_FIELDS = [
+  {
+    name: 'title',
+    default: '',
+  },
+  {
+    name: 'createDate',
+    default: new Date(),
+  },
+  {
+    name: 'pubDate',
+    default: '',
+  },
+  {
+    name: 'tags',
+    default: '[]',
+  },
+  {
+    name: 'description',
+    default: '',
+  },
+  {
+    name: 'draft',
+    default: true,
+  }
+]
+
+const [ title, description ] = argv.slice(2);
+
+
+function genPost(params = {}) {
+  const itemList = POST_FIELDS.map(info => {
+    return `${info.name}: ${params[info.name] || info.default}`
+  })
+  return `---\n${itemList.join('\n')}\n---`
+}
+
+const postData = genPost({
+  title,
+  description
+})
+
+const newBlogPath = `${BLOG_PATH}${title || 'new-post'}.md`;
+writeFileSync(newBlogPath, postData)
+spawn('code', [newBlogPath])
+
