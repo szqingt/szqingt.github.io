36f81bff6e7bfa5536d47390a16786a488714f04
never_1992 <szqingt@gmail.com>
2023-04-24 00:48:55 +0800

    feat: code block title

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,19 @@ import mdx from '@astrojs/mdx';
 import ShikiRemarkPlugin from 'remark-shiki-plugin';
 import sitemap from '@astrojs/sitemap';
 
+function customerHtmlHandle(code, html, theme) {
+  const titleReg = /(?:\s|^)title\s*=\s*(["'])(.*?)(?<!\\)\1/
+  const match = (code.meta || '').match(titleReg)
+  const [_, __, titleValue] = Array.from(match || [])
+  if (titleValue) {
+    return `<figure class="${theme}-code-container">
+      <figcaption class="header"><span class="tite">${titleValue}</span></figcaption>${html}
+    </figure>`
+  }
+  
+  return html
+} 
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://szqingt.github.io',
@@ -19,7 +32,8 @@ export default defineConfig({
       [ShikiRemarkPlugin, {
         themes: ['vitesse-dark', 'vitesse-light'],
         generateMultiCode: true,
-        highlightLines: true
+        highlightLines: true,
+        customerHtmlHandle
       }]
     ]
 	},
