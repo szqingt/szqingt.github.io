7c035b6debecaab70bc1fbde35393b7c59933909
never_1992 <szqingt@gmail.com>
2023-05-10 00:33:57 +0800

    chore: some adjust

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Qt blog';
+export const SITE_DESCRIPTION = 'Welcome to my blog!';
 
 
 export const REDIRECT_INFO_LIST = [
