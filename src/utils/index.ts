3447f7a9805f196965237ce34497c42622d501bb
never_1992 <szqingt@gmail.com>
2023-06-05 00:38:31 +0800

    feat: fancy link dark mode

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,14 +3,24 @@ import { getCollection } from 'astro:content';
 
 let themeMode = localStorage.getItem('color-scheme') || 'auto'
 
-export function toggleTheme() {
-  const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+export function getPrefersDark() {
+  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+}
 
+export function getThemeClassWithMode(mode: string) {
+  const prefersDark = getPrefersDark()
+  return mode === 'auto' ? prefersDark ? 'dark' : 'light' : mode
+}
+
+export function toggleTheme(cb?: Function) {
+  const prefersDark = getPrefersDark()
+  
   function setMode(mode: string) {
-    document.documentElement.classList.remove(themeMode === 'auto' ? prefersDark ? 'dark' : 'light' : themeMode)
+    document.documentElement.classList.remove(getThemeClassWithMode(themeMode))
     themeMode = mode
-    document.documentElement.classList.add(themeMode === 'auto' ? prefersDark ? 'dark' : 'light' : themeMode)
+    document.documentElement.classList.add(getThemeClassWithMode(themeMode))
     localStorage.setItem('color-scheme', themeMode)
+    cb && cb(themeMode)
   }
 
   // 系统推荐黑暗 当前是不是light
@@ -24,6 +34,10 @@ export function toggleTheme() {
     return
   }
   setMode('auto')
+}
+
+export function getThemeMode() {
+  return themeMode
 }
 
 export function simpleCalcReadingTime(content: string): string {
